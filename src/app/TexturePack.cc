088d5fc8ee0c153ddc4c55ef7a2705e75b10d36c
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2020-12-27 18:09:13 +0100

    fix: Handled TODO statement to prevent feature creep. A warning will remain us of implementing the behavior if needed

--- src/app/TexturePack.cc
+++ src/app/TexturePack.cc
@@ -29,6 +29,16 @@ namespace tdef {
     p.sSize = pack.sSize;
     p.layout = pack.layout;
 
+    // Add a warning in case the layout has more than
+    // 1 row: we don't really handle it right now.
+    if (p.layout.y > 1) {
+      log(
+        "Registering pack \"" + pack.file + "\" with layout " +
+        toString(p.layout) + " having more than one row",
+        utils::Level::Warning
+      );
+    }
+
     p.res = new olc::Decal(spr);
 
     unsigned id = m_packs.size();
@@ -55,7 +65,6 @@ namespace tdef {
 
     const Pack& tp = m_packs[s.pack];
 
-    // TODO: Replace with actual resolution of type.
     olc::vi2d sCoords = spriteCoords(tp, olc::vi2d(s.sprite, 0), s.id);
     pge->DrawPartialDecal(p, tp.res, sCoords, tp.sSize, scale, s.tint);
   }

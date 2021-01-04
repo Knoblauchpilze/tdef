e691879abfcce7da148ae53ca2c0c579ecc6947a
Knoblauchpilze <prolific-programmer@tuta.com>
2021-01-04 17:19:17 +0100

    feat: Provided basic implementation of a factory to create mobs

--- src/game/MobData.cc
+++ src/game/MobData.cc
@@ -1,149 +1,294 @@
 
 # include "MobData.hh"
 
-namespace mobs {
+namespace tdef {
+  namespace mobs {
 
-  namespace regular {
-    static const bool poisonable = true;
-    static const bool slowable = true;
-    static const bool stunnable = true;
+    namespace regular {
+      static const bool poisonable = true;
+      static const bool slowable = true;
+      static const bool stunnable = true;
 
-    static const float bounty = 1.0f;
-    static const float speed = 1.0f;
-    static const float lives = 1.0f;
+      static const float bounty = 1.0f;
+      static const float speed = 1.0f;
+      static const float lives = 1.0f;
 
-    static const float health = 1.0f;
-    static const float shield = 0.0f;
+      static const float health = 1.0f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Regular);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace fast {
+      static const bool poisonable = true;
+      static const bool slowable = true;
+      static const bool stunnable = true;
+
+      static const float bounty = 0.7f;
+      static const float speed = 1.25f;
+      static const float lives = 1.0f;
+
+      static const float health = 0.8f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Fast);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace strong {
+      static const bool poisonable = true;
+      static const bool slowable = true;
+      static const bool stunnable = true;
+
+      static const float bounty = 1.2f;
+      static const float speed = 1.0f;
+      static const float lives = 1.0f;
+
+      static const float health = 1.2f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Strong);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace heli {
+      static const bool poisonable = true;
+      static const bool slowable = true;
+      static const bool stunnable = false;
+
+      static const float bounty = 1.0f;
+      static const float speed = 1.0f;
+      static const float lives = 1.0f;
+
+      static const float health = 1.0f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Heli);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace jet {
+      static const bool poisonable = true;
+      static const bool slowable = false;
+      static const bool stunnable = false;
+
+      static const float bounty = 1.0f;
+      static const float speed = 1.20f;
+      static const float lives = 1.0f;
+
+      static const float health = 0.8f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Jet);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace armored {
+      static const bool poisonable = true;
+      static const bool slowable = true;
+      static const bool stunnable = true;
+
+      static const float bounty = 1.1f;
+      static const float speed = 0.95f;
+      static const float lives = 1.0f;
+
+      static const float health = 1.1f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Armored);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace healer {
+      static const bool poisonable = true;
+      static const bool slowable = true;
+      static const bool stunnable = true;
+
+      static const float bounty = 1.15f;
+      static const float speed = 1.0f;
+      static const float lives = 1.0f;
+
+      static const float health = 0.95f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Healer);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace toxic {
+      static const bool poisonable = false;
+      static const bool slowable = true;
+      static const bool stunnable = true;
+
+      static const float bounty = 1.0f;
+      static const float speed = 1.0f;
+      static const float lives = 1.0f;
+
+      static const float health = 1.0f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Toxic);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace icy {
+      static const bool poisonable = true;
+      static const bool slowable = false;
+      static const bool stunnable = false;
+
+      static const float bounty = 0.8f;
+      static const float speed = 1.05f;
+      static const float lives = 1.0f;
+
+      static const float health = 0.8f;
+      static const float shield = 0.4f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Icy);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace fighter {
+      static const bool poisonable = true;
+      static const bool slowable = true;
+      static const bool stunnable = true;
+
+      static const float bounty = 1.0f;
+      static const float speed = 1.0f;
+      static const float lives = 1.0f;
+
+      static const float health = 1.0f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Fighter);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
+    namespace light {
+      static const bool poisonable = true;
+      static const bool slowable = true;
+      static const bool stunnable = true;
+
+      static const float bounty = 1.1f;
+      static const float speed = 1.0f;
+      static const float lives = 1.0f;
+
+      static const float health = 0.95f;
+      static const float shield = 0.0f;
+
+      Mob::MProps
+      generateProps(const utils::Point2f& p, int /*level*/) noexcept {
+        Mob::MProps pp = Mob::newProps(p, Type::Light);
+
+        pp.health = health;
+
+        pp.speed = speed;
+        pp.bounty = bounty;
+
+        return pp;
+      }
+
+    }
+
   }
-
-  namespace fast {
-    static const bool poisonable = true;
-    static const bool slowable = true;
-    static const bool stunnable = true;
-
-    static const float bounty = 0.7f;
-    static const float speed = 1.25f;
-    static const float lives = 1.0f;
-
-    static const float health = 0.8f;
-    static const float shield = 0.0f;
-  }
-
-  namespace strong {
-    static const bool poisonable = true;
-    static const bool slowable = true;
-    static const bool stunnable = true;
-
-    static const float bounty = 1.2f;
-    static const float speed = 1.0f;
-    static const float lives = 1.0f;
-
-    static const float health = 1.2f;
-    static const float shield = 0.0f;
-  }
-
-  namespace heli {
-    static const bool poisonable = true;
-    static const bool slowable = true;
-    static const bool stunnable = false;
-
-    static const float bounty = 1.0f;
-    static const float speed = 1.0f;
-    static const float lives = 1.0f;
-
-    static const float health = 1.0f;
-    static const float shield = 0.0f;
-  }
-
-  namespace jet {
-    static const bool poisonable = true;
-    static const bool slowable = false;
-    static const bool stunnable = false;
-
-    static const float bounty = 1.0f;
-    static const float speed = 1.20f;
-    static const float lives = 1.0f;
-
-    static const float health = 0.8f;
-    static const float shield = 0.0f;
-  }
-
-  namespace armored {
-    static const bool poisonable = true;
-    static const bool slowable = true;
-    static const bool stunnable = true;
-
-    static const float bounty = 1.1f;
-    static const float speed = 0.95f;
-    static const float lives = 1.0f;
-
-    static const float health = 1.1f;
-    static const float shield = 0.0f;
-  }
-
-  namespace healer {
-    static const bool poisonable = true;
-    static const bool slowable = true;
-    static const bool stunnable = true;
-
-    static const float bounty = 1.15f;
-    static const float speed = 1.0f;
-    static const float lives = 1.0f;
-
-    static const float health = 0.95f;
-    static const float shield = 0.0f;
-  }
-
-  namespace toxic {
-    static const bool poisonable = false;
-    static const bool slowable = true;
-    static const bool stunnable = true;
-
-    static const float bounty = 1.0f;
-    static const float speed = 1.0f;
-    static const float lives = 1.0f;
-
-    static const float health = 1.0f;
-    static const float shield = 0.0f;
-  }
-
-  namespace icy {
-    static const bool poisonable = true;
-    static const bool slowable = false;
-    static const bool stunnable = false;
-
-    static const float bounty = 0.8f;
-    static const float speed = 1.05f;
-    static const float lives = 1.0f;
-
-    static const float health = 0.8f;
-    static const float shield = 0.4f;
-  }
-
-  namespace fighter {
-    static const bool poisonable = true;
-    static const bool slowable = true;
-    static const bool stunnable = true;
-
-    static const float bounty = 1.0f;
-    static const float speed = 1.0f;
-    static const float lives = 1.0f;
-
-    static const float health = 1.0f;
-    static const float shield = 0.0f;
-  }
-
-  namespace light {
-    static const bool poisonable = true;
-    static const bool slowable = true;
-    static const bool stunnable = true;
-
-    static const float bounty = 1.1f;
-    static const float speed = 1.0f;
-    static const float lives = 1.0f;
-
-    static const float health = 0.95f;
-    static const float shield = 0.0f;
-  }
-
 }
--- src/game/MobFactory.cc
+++ src/game/MobFactory.cc
@@ -1,6 +1,42 @@
 
 # include "MobFactory.hh"
+# include "MobData.hh"
 
 namespace tdef {
+  namespace mobs {
 
+    Mob::MProps
+    generateProps(const Type& type,
+                  const utils::Point2f& p,
+                  int level)
+    {
+      switch (type) {
+        case Type::Fast:
+          return fast::generateProps(p, level);
+        case Type::Strong:
+          return strong::generateProps(p, level);
+        case Type::Heli:
+          return heli::generateProps(p, level);
+        case Type::Jet:
+          return jet::generateProps(p, level);
+        case Type::Armored:
+          return armored::generateProps(p, level);
+        case Type::Healer:
+          return healer::generateProps(p, level);
+        case Type::Toxic:
+          return toxic::generateProps(p, level);
+        case Type::Icy:
+          return icy::generateProps(p, level);
+        case Type::Fighter:
+          return fighter::generateProps(p, level);
+        case Type::Light:
+          return light::generateProps(p, level);
+        case Type::Regular:
+        default:
+          // Assume default is regular mob.
+          return regular::generateProps(p, level);
+      }
+    }
+
+  }
 }
